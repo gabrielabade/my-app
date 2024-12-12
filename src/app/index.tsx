78487501e4176aaa33f0e826ce2aa36ab789da55
gabrielabade <gabriela.bade@hotmail.com>
2024-12-12 17:57:55 -0300

    create home/welcome

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,14 @@
 import { View, Text } from 'react-native'
 
+import { Steps } from '@/components/steps'
+import { Welcome } from "@/components/welcome"
+
+
 export default function Index() {
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-    >
-      <Text style={{ fontSize: 22 }}>Hello React Native</Text>
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
+      <Steps />
     </View>
   )
 }
