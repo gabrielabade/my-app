fc0ae0b4dec6ac3df493e40503c665906b429104
gabrielabade <gabriela.bade@hotmail.com>
2024-12-13 10:22:39 -0300

    create icon

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,5 +1,5 @@
 import { TouchableOpacity, TouchableOpacityProps, Text, TextProps, ActivityIndicator } from "react-native";
-
+import { IconProps as TablerIconProps } from "@tabler/icons-react-native"
 import { s } from "./styles"
 import { colors } from "@/styles/theme";
 
@@ -25,5 +25,14 @@ function Title({ children }: TextProps) {
   return <Text style={s.title}>{children}</Text>
 }
 
+type IconProps = {
+  icon: React.ComponentType<TablerIconProps>
+}
+function Icon({ icon: Icon }: IconProps) {
+  return <Icon size={24} color={colors.gray[100]} />
+}
+
 Button.Title = Title
+Button.Icon = Icon
+
 export { Button }
